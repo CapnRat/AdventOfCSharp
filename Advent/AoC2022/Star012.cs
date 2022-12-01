c5d11b81fa16587723de12b3d05d7bb1db5083aa
Shawn White <shawn@capnrat.com>
2022-12-01 09:50:14 +0100

    Solution 2022-01-2

---
+++ Advent/AoC2022/Star012.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advent.Common;
+
+namespace Advent.AoC2022
+{
+    [Solution(22, 1, 2)]
+    public class Star012 : Solution<int>
+    {
+        public override int Run(string input)
+        {
+            var amounts = new List<int>{0};
+            foreach (var line in Utility.InputToLines(input))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    amounts.Add(0);
+                    continue;
+                }
+
+                amounts[^1] += int.Parse(line);
+            }
+            
+            amounts.Sort();
+
+            return amounts.ToArray()[^3..].Sum();
+        }
+    }
+}
---
+++ AdventTests/AoC2022/Star012Test.cs
@@ -0,0 +1,27 @@
+﻿using Advent.AoC2022;
+using NUnit.Framework;
+
+namespace AdventTests.AoC2022
+{
+    public class Star012Test : SolutionTest<Star012>
+    {
+        [TestCase(@"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000", 45000)]
+        public void ExampleTests(string input, int expected)
+        {
+            Run(input, expected);
+        }
+    }
+}
