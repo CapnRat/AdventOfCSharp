1c790caca7500fc707435e6929f561cbe5db44bc
Shawn White <shawn@capnrat.com>
2022-12-01 09:35:25 +0100

    Solution 2022-01-1

---
+++ Advent/AoC2022/Star011.cs
@@ -0,0 +1,29 @@
+﻿using Advent.Common;
+
+namespace Advent.AoC2022
+{
+    [Solution(22, 01, 01)]
+    public class Star011 : Solution<int>
+    {
+        public override int Run(string input)
+        {
+            int max = 0;
+            int current = 0;
+            foreach (var line in Utility.InputToLines(input))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    if (current > max)
+                        max = current;
+                    current = 0;
+                    continue;
+                }
+
+                var amount = int.Parse(line);
+                current += amount;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ AdventTests/AoC2022/Star011Test.cs
@@ -0,0 +1,27 @@
+﻿using Advent.AoC2022;
+using NUnit.Framework;
+
+namespace AdventTests.AoC2022
+{
+    public class Star011Test : SolutionTest<Star011>
+    {
+        [TestCase(@"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000", 24000)]
+        public void ExampleTests(string input, int expected)
+        {
+            Run(input, expected);
+        }
+    }
+}
