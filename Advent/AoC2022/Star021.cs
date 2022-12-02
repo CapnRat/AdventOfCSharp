b398f4c40265a41e67e8b1ad891be55f948727bc
Shawn White <shawn@capnrat.com>
2022-12-02 10:12:31 +0100

    Solution 2022-02-1

---
+++ Advent/AoC2022/Star021.cs
@@ -0,0 +1,30 @@
+﻿using Advent.Common;
+
+namespace Advent.AoC2022
+{
+    [Solution(22, 2, 1)]
+    public class Star021 : Solution<int>
+    {
+        public override int Run(string input)
+        {
+            int score = 0;
+
+            const int asciiDiff = 'X' - 'A';
+            
+            foreach (var line in Utility.InputToLines(input))
+            {
+                int opponentAction = line[0] + asciiDiff;
+                int strategyAction = line[2];
+
+                if (opponentAction == strategyAction)
+                    score += 3;
+                else if (strategyAction - opponentAction == 1 || strategyAction - opponentAction == -2)
+                    score += 6;
+
+                score += strategyAction - 'X' + 1;
+            }
+
+            return score;
+        }
+    }
+}
---
+++ AdventTests/AoC2022/Star021Test.cs
@@ -0,0 +1,16 @@
+﻿using Advent.AoC2022;
+using NUnit.Framework;
+
+namespace AdventTests.AoC2022
+{
+    public class Star021Test : SolutionTest<Star021>
+    {
+        [TestCase(@"A Y
+B X
+C Z", 15)]
+        public void ExampleTests(string input, int expected)
+        {
+            Run(input, expected);
+        }
+    }
+}
