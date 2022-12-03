cc7b2f93fbccd509a155c7158306c3f2de2c0f0e
Shawn White <shawn@capnrat.com>
2022-12-03 13:00:20 +0100

    Solution 2022-03-2

---
+++ Advent/AoC2022/Star032.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Advent.Common;
+
+namespace Advent.AoC2022
+{
+    [Solution(22, 3,2)]
+    public class Star032 : Solution<int>
+    {
+        public override int Run(string input)
+        {
+            int priority = 0;
+            
+            var lines = Utility.InputToLines(input).ToArray();
+            for (int i = 0; i < lines.Length; i += 3)
+            {
+                var c = lines[i].Intersect(lines[i + 1]).Intersect(lines[i + 2]).First();
+                
+                if (char.IsLower(c))
+                    priority += c - 'a' + 1;
+                else
+                    priority += c - 'A' + 27;
+            }
+
+            return priority;
+        }
+    }
+}
---
+++ AdventTests/AoC2022/Star032Test.cs
@@ -0,0 +1,19 @@
+﻿using Advent.AoC2022;
+using NUnit.Framework;
+
+namespace AdventTests.AoC2022
+{
+    public class Star032Test : SolutionTest<Star032>
+    {
+        [TestCase(@"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw", 70)]
+        public void ExampleTests(string input, int expected)
+        {
+            Run(input, expected);
+        }
+    }
+}
