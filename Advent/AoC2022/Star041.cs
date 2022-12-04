1d96d77a3cbf0ffc3e61664c03c51d07c51a63b6
Shawn White <shawn@capnrat.com>
2022-12-04 10:24:23 +0100

    Solution 2022-04-1

---
+++ Advent/AoC2022/Star041.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advent.Common;
+
+namespace Advent.AoC2022
+{
+    [Solution(22, 4,1)]
+    public class Star041 : Solution<int>
+    {
+        private IEnumerable<int> SplitToIDs(string split)
+        {
+            var range = split.Split('-').Select(int.Parse).ToArray();
+            return Enumerable.Range(range[0], range[1] + 1 - range[0]);
+        }
+        
+        public override int Run(string input)
+        {
+            return Utility.InputTo(l =>
+            {
+                var pairs = l.Split(',');
+                return (SplitToIDs(pairs[0]), SplitToIDs(pairs[1]));
+            }, input).Where(pair =>
+            {
+                var intersectCount = pair.Item1.Intersect(pair.Item2).Count();
+                return (intersectCount == pair.Item1.Count() || intersectCount == pair.Item2.Count());
+            }).Count();
+        }
+    }
+}
---
+++ AdventTests/AoC2022/Star041Test.cs
@@ -0,0 +1,19 @@
+﻿using Advent.AoC2022;
+using NUnit.Framework;
+
+namespace AdventTests.AoC2022
+{
+    public class Star041Test : SolutionTest<Star041>
+    {
+        [TestCase(@"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8", 2)]
+        public void ExampleTests(string input, int expected)
+        {
+            Run(input, expected);
+        }
+    }
+}
