1cc269896f15fbd837e7d5a9eada7122061f02f3
Shawn White <shawn@capnrat.com>
2022-12-06 10:02:15 +0100

    Solution 2022-06-1

---
+++ Advent/AoC2022/Star061.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Advent.Common;
+
+namespace Advent.AoC2022
+{
+    [Solution(22, 6, 1)]
+    public class Star061 : Solution<int>
+    {
+        public override int Run(string input)
+        {
+            for (int i = 4; i <= input.Length; i++)
+            {
+                if (input[(i - 4)..i].Distinct().Count() == 4)
+                    return i;
+            }
+
+            throw new ApplicationException("Failed to find starting packet.");
+        }
+    }
+}
---
+++ AdventTests/AoC2022/Star061Test.cs
@@ -0,0 +1,18 @@
+﻿using Advent.AoC2022;
+using NUnit.Framework;
+
+namespace AdventTests.AoC2022
+{
+    public class Star061Test : SolutionTest<Star061>
+    {
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        public void ExampleTests(string input, int expected)
+        {
+            Run(input, expected);
+        }
+    }
+}
