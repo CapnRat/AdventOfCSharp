4c97f36636d1aa40e148918e277e03003edcb19b
Shawn White <shawn@capnrat.com>
2022-12-09 12:36:39 +0100

    Solution 2022-09-1

---
+++ Advent/AoC2022/Star091.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Advent.Common;
+
+namespace Advent.AoC2022
+{
+    [Solution(22, 9, 1)]
+    public class Star091 : Solution<int>
+    {
+        public struct Movement
+        {
+            public (int X, int Y) Direction;
+            public int Distance;
+        }
+        
+        public struct Position
+        {
+            public int X;
+            public int Y;
+
+            public void Add((int X, int Y) offset)
+            {
+                X += offset.X;
+                Y += offset.Y;
+            }
+        }
+        
+        public override int Run(string input)
+        {
+            var visited = new HashSet<Position>();
+
+            var head = new Position();
+            var tail = new Position();
+
+            foreach (var movement in Utility.InputTo(l =>
+                     {
+                         var splits = l.Split(' ');
+                         return new Movement{ Direction = splits[0] switch
+                         {
+                             "U" => ( 0,  1),
+                             "L" => (-1,  0),
+                             "D" => ( 0, -1),
+                             "R" => ( 1,  0),
+                             _ => throw new ArgumentException()
+                         }, Distance = int.Parse(splits[1])};
+                     }, input))
+            {
+                for (int i = 0; i < movement.Distance; i++)
+                {
+                    var oldHead = head;
+                    head.Add(movement.Direction);
+
+                    if (Math.Abs(head.X - tail.X) > 1 || Math.Abs(head.Y - tail.Y) > 1)
+                        tail = oldHead;
+
+                    visited.Add(tail);
+                }
+            }
+
+            return visited.Count;
+        }
+    }
+}
---
+++ AdventTests/AoC2022/Star091Test.cs
@@ -0,0 +1,21 @@
+﻿using Advent.AoC2022;
+using NUnit.Framework;
+
+namespace AdventTests.AoC2022
+{
+    public class Star091Test : SolutionTest<Star091>
+    {
+        [TestCase(@"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2", 13)]
+        public void ExampleTests(string input, int expected)
+        {
+            Run(input, expected);
+        }
+    }
+}
